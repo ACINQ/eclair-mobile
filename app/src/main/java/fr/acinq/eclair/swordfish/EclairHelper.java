cdf680539cda2f2673819952ddf9b43cbd0ea60e
dpad85 <dpad85@users.noreply.github.com>
2017-07-03 16:38:47 +0200

    Catching timeout when starting eclair to prevent crash

--- app/src/main/java/fr/acinq/eclair/swordfish/EclairHelper.java
+++ app/src/main/java/fr/acinq/eclair/swordfish/EclairHelper.java
@@ -3,6 +3,7 @@ package fr.acinq.eclair.swordfish;
 import android.util.Log;
 
 import java.io.File;
+import java.util.concurrent.TimeoutException;
 
 import akka.actor.ActorRef;
 import akka.actor.Props;
@@ -12,8 +13,8 @@ import fr.acinq.eclair.payment.PaymentEvent;
 import fr.acinq.eclair.router.NetworkEvent;
 
 public class EclairHelper {
+  private static final String TAG = "EclairHelper";
   private static EclairHelper mInstance = null;
-
   private Setup setup;
   private ActorRef guiUpdater;
 
@@ -22,14 +23,18 @@ public class EclairHelper {
   private EclairHelper(File f) {
     Log.i("Eclair Helper", "Accessing Eclair Setup with datadir in " + f.getAbsolutePath());
     File data = new File(f, "eclair-wallet-data");
-    System.setProperty("eclair.node-alias", "sw-newt");
-    Setup setup = new Setup(data, "system");
-    this.setup = setup;
-    this.guiUpdater = this.setup.system().actorOf(Props.create(EclairEventService.class));
-    setup.system().eventStream().subscribe(guiUpdater, ChannelEvent.class);
-    setup.system().eventStream().subscribe(guiUpdater, PaymentEvent.class);
-    setup.system().eventStream().subscribe(guiUpdater, NetworkEvent.class);
-    setup.boostrap();
+    System.setProperty("eclair.node-alias", "sw-ripley");
+    try {
+      Setup setup = new Setup(data, "system");
+      this.setup = setup;
+      this.guiUpdater = this.setup.system().actorOf(Props.create(EclairEventService.class));
+      setup.system().eventStream().subscribe(guiUpdater, ChannelEvent.class);
+      setup.system().eventStream().subscribe(guiUpdater, PaymentEvent.class);
+      setup.system().eventStream().subscribe(guiUpdater, NetworkEvent.class);
+      setup.boostrap();
+    } catch (Exception e) {
+      Log.e(TAG, "Failed to start eclair", e);
+    }
   }
 
   public static boolean hasInstance() {
--- app/src/main/java/fr/acinq/eclair/swordfish/activity/LauncherActivity.java
+++ app/src/main/java/fr/acinq/eclair/swordfish/activity/LauncherActivity.java
@@ -3,10 +3,11 @@ package fr.acinq.eclair.swordfish.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import fr.acinq.eclair.swordfish.EclairHelper;
 import fr.acinq.eclair.swordfish.R;
-import fr.acinq.eclair.swordfish.StartupTask;
+import fr.acinq.eclair.swordfish.tasks.StartupTask;
 
 public class LauncherActivity extends AppCompatActivity implements StartupTask.AsyncSetupResponse {
 
@@ -19,9 +20,13 @@ public class LauncherActivity extends AppCompatActivity implements StartupTask.A
 
   @Override
   public void processFinish(String output) {
-    Intent intent = new Intent(this, HomeActivity.class);
-    startActivity(intent);
-    finish();
+    if (EclairHelper.hasInstance()) {
+      Intent intent = new Intent(this, HomeActivity.class);
+      startActivity(intent);
+      finish();
+    } else {
+      Toast.makeText(this, "Could not start eclair", Toast.LENGTH_LONG).show();
+    }
   }
 }
 
