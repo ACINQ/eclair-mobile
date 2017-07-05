407c9c40319b9c8d3e9cb4b31819f969db64ab2f
dpad85 <dpad85@users.noreply.github.com>
2017-07-05 18:52:44 +0200

    Updated look of payment feedback activities

--- app/src/main/java/fr/acinq/eclair/swordfish/activity/PaymentFailureActivity.java
+++ app/src/main/java/fr/acinq/eclair/swordfish/activity/PaymentFailureActivity.java
@@ -2,13 +2,13 @@ package fr.acinq.eclair.swordfish.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-import fr.acinq.bitcoin.MilliSatoshi;
 import fr.acinq.eclair.swordfish.R;
-import fr.acinq.eclair.swordfish.customviews.CoinAmountView;
 
 public class PaymentFailureActivity extends AppCompatActivity {
 
@@ -17,25 +17,36 @@ public class PaymentFailureActivity extends AppCompatActivity {
   public static final String EXTRA_PAYMENTFAILURE_CAUSE = "fr.acinq.eclair.swordfish.EXTRA_PAYMENTFAILURE_CAUSE";
   private static final String TAG = "PaymentFailureActivity";
 
+  private Handler dismissHandler;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_payment_failure);
 
     Intent intent = getIntent();
-    Long amount = intent.getLongExtra(EXTRA_PAYMENTFAILURE_AMOUNT, 0);
     String desc = intent.getStringExtra(EXTRA_PAYMENTFAILURE_DESC);
     String cause = intent.getStringExtra(EXTRA_PAYMENTFAILURE_CAUSE);
 
-    CoinAmountView amountView = (CoinAmountView) findViewById(R.id.paymentfailure_amount);
-    amountView.setAmountMsat(new MilliSatoshi(amount));
     TextView descView = (TextView) findViewById(R.id.paymentfailure_desc);
     descView.setText(desc);
     TextView causeView = (TextView) findViewById(R.id.paymentfailure_cause);
     causeView.setText(cause);
+
+    ImageView mSadImage = (ImageView) findViewById(R.id.paymentfailure_sad);
+    mSadImage.setAlpha(0);
+    mSadImage.animate().alpha(1).setStartDelay(250).setDuration(500).start();
+
+    dismissHandler = new Handler();
+    dismissHandler.postDelayed(new Runnable() {
+      public void run() {
+        finish();
+      }
+    }, 3500);
   }
 
   public void failure_dismiss(View view) {
+    dismissHandler.removeCallbacks(null);
     finish();
   }
 }
--- app/src/main/java/fr/acinq/eclair/swordfish/activity/PaymentSuccessActivity.java
+++ app/src/main/java/fr/acinq/eclair/swordfish/activity/PaymentSuccessActivity.java
@@ -1,14 +1,14 @@
 package fr.acinq.eclair.swordfish.activity;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-import fr.acinq.bitcoin.MilliSatoshi;
 import fr.acinq.eclair.swordfish.R;
-import fr.acinq.eclair.swordfish.customviews.CoinAmountView;
 
 public class PaymentSuccessActivity extends AppCompatActivity {
 
@@ -16,22 +16,39 @@ public class PaymentSuccessActivity extends AppCompatActivity {
   public static final String EXTRA_PAYMENTSUCCESS_DESC = "fr.acinq.eclair.swordfish.EXTRA_PAYMENTSUCCESS_DESC";
   private static final String TAG = "PaymentSuccessActivity";
 
+  private Handler dismissHandler;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_payment_success);
 
     Intent intent = getIntent();
-    Long amount = intent.getLongExtra(EXTRA_PAYMENTSUCCESS_AMOUNT, 0);
     String desc = intent.getStringExtra(EXTRA_PAYMENTSUCCESS_DESC);
 
-    CoinAmountView amountView = (CoinAmountView) findViewById(R.id.paymentsuccess_amount);
-    amountView.setAmountMsat(new MilliSatoshi(amount));
     TextView descView = (TextView) findViewById(R.id.paymentsuccess_desc);
     descView.setText(desc);
+
+    ImageView mCircleImage = (ImageView) findViewById(R.id.paymentsuccess_circle);
+    ImageView mCheckImage = (ImageView) findViewById(R.id.paymentsuccess_check);
+    mCircleImage.setAlpha(0);
+    mCircleImage.setTranslationY(60);
+    mCheckImage.setAlpha(0);
+    mCheckImage.setScaleX(0.6f);
+    mCheckImage.setScaleY(0.6f);
+    mCircleImage.animate().alpha(1).translationY(0).setStartDelay(200).setDuration(250).start();
+    mCheckImage.animate().alpha(1).scaleY(1).scaleX(1).setStartDelay(350).setDuration(100).start();
+
+    dismissHandler = new Handler();
+    dismissHandler.postDelayed(new Runnable() {
+      public void run() {
+        finish();
+      }
+    }, 3500);
   }
 
   public void success_dismiss(View view) {
+    dismissHandler.removeCallbacks(null);
     finish();
   }
 }
