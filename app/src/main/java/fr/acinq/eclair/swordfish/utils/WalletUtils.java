ef7f99462718f9e11d066b63d471f2837c2db35e
dpad85 <dpad85@users.noreply.github.com>
2017-07-12 11:13:15 +0200

    Updated explorer api url

--- app/src/main/java/fr/acinq/eclair/swordfish/utils/WalletUtils.java
+++ app/src/main/java/fr/acinq/eclair/swordfish/utils/WalletUtils.java
@@ -5,14 +5,16 @@ import android.net.Uri;
 import android.view.View;
 
 public class WalletUtils {
-  public final static String EXPLORER_TRANSACTION_URI = "https://tbtc.blockr.io/tx/info/";
+  public final static String EXPLORER_TRANSACTION_URI = "https://api.blockcypher.com/v1/btc/test3/txs/";
 
   public final static View.OnClickListener getOpenTxListener(final String txId) {
     return new View.OnClickListener() {
       @Override
       public void onClick(View v) {
-        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(WalletUtils.EXPLORER_TRANSACTION_URI + txId));
-        v.getContext().startActivity(browserIntent);
+        if (txId != null && !txId.trim().isEmpty()) {
+          Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(WalletUtils.EXPLORER_TRANSACTION_URI + txId));
+          v.getContext().startActivity(browserIntent);
+        }
       }
     };
   }
