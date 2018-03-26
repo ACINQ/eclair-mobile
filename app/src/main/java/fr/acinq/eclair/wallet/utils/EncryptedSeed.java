c91f8c5c93bd0450aa8f02605203283850593c6d
dpad85 <dpadiou@gmail.com>
2018-03-26 16:34:03 +0200

    Using stream to read a serialized encrypted seed

--- app/src/main/java/fr/acinq/eclair/wallet/utils/EncryptedSeed.java
+++ app/src/main/java/fr/acinq/eclair/wallet/utils/EncryptedSeed.java
@@ -2,18 +2,17 @@ package fr.acinq.eclair.wallet.utils;
 
 import com.tozny.crypto.android.AesCbcWithIntegrity;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 
 public class EncryptedSeed {
 
-  private static final int VERSION_LENGTH = 1;
+  public final static byte SEED_FILE_VERSION_1 = 1;
   private static final int SALT_LENGTH_V1 = 128;
   private static final int IV_LENGTH_V1 = 16;
   private static final int MAC_LENGTH_V1 = 32;
-  public final static byte SEED_FILE_VERSION_1 = 1;
-
   private final int version;
   private final byte[] salt;
   private final AesCbcWithIntegrity.CipherTextIvMac civ;
@@ -22,6 +21,46 @@ public class EncryptedSeed {
     this.version = version;
     this.salt = salt;
     this.civ = civ;
+  }
+
+  /**
+   * Encrypt a non encrypted seed with AES CBC and return an object containing the encrypted seed.
+   *
+   * @param seed     the seed to encrypt
+   * @param password the password encrypting the seed
+   * @param version  the version describing the serialization to use for the EncryptedSeed object
+   * @return a encrypted seed ready to be serialized
+   * @throws GeneralSecurityException
+   */
+  static EncryptedSeed encrypt(final byte[] seed, final String password, final int version) throws GeneralSecurityException {
+    final byte[] salt = AesCbcWithIntegrity.generateSalt();
+    final AesCbcWithIntegrity.SecretKeys sk = AesCbcWithIntegrity.generateKeyFromPassword(password, salt);
+    final AesCbcWithIntegrity.CipherTextIvMac civ = AesCbcWithIntegrity.encrypt(seed, sk);
+    return new EncryptedSeed(version, salt, civ);
+  }
+
+  /**
+   * Read an array of byte and deserializes it as an EncryptedSeed object.
+   *
+   * @param serialized array to deserialize
+   * @return
+   */
+  public static EncryptedSeed read(final byte[] serialized) {
+    final ByteArrayInputStream stream = new ByteArrayInputStream(serialized);
+    final int version = stream.read();
+    if (version == SEED_FILE_VERSION_1) {
+      final byte[] salt = new byte[SALT_LENGTH_V1];
+      stream.read(salt, 0, SALT_LENGTH_V1);
+      final byte[] iv = new byte[IV_LENGTH_V1];
+      stream.read(iv, 0, IV_LENGTH_V1);
+      final byte[] mac = new byte[MAC_LENGTH_V1];
+      stream.read(mac, 0, MAC_LENGTH_V1);
+      final byte[] cipher = new byte[stream.available()];
+      stream.read(cipher, 0, stream.available());
+      return new EncryptedSeed(version, salt, new AesCbcWithIntegrity.CipherTextIvMac(cipher, iv, mac));
+    } else {
+      throw new RuntimeException("unhandled encrypted seed file version");
+    }
   }
 
   /**
@@ -54,65 +93,6 @@ public class EncryptedSeed {
   byte[] decrypt(final String password) throws GeneralSecurityException {
     final AesCbcWithIntegrity.SecretKeys sk = AesCbcWithIntegrity.generateKeyFromPassword(password, salt);
     return AesCbcWithIntegrity.decrypt(civ, sk);
-  }
-
-  /**
-   * Encrypt a non encrypted seed with AES CBC and return an object containing the encrypted seed.
-   * @param seed the seed to encrypt
-   * @param password the password encrypting the seed
-   * @param version the version describing the serialization to use for the EncryptedSeed object
-   * @return a encrypted seed ready to be serialized
-   * @throws GeneralSecurityException
-   */
-  static EncryptedSeed encrypt(final byte[] seed, final String password, final int version) throws GeneralSecurityException {
-    final byte[] salt = AesCbcWithIntegrity.generateSalt();
-    final AesCbcWithIntegrity.SecretKeys sk = AesCbcWithIntegrity.generateKeyFromPassword(password, salt);
-    final AesCbcWithIntegrity.CipherTextIvMac civ = AesCbcWithIntegrity.encrypt(seed, sk);
-    return new EncryptedSeed(version, salt, civ);
-  }
-
-  /**
-   * Read an array of byte and deserializes it as an EncryptedSeed object.
-   * @param serialized array to deserialize
-   * @return
-   */
-  public static EncryptedSeed read(final byte[] serialized) {
-    final byte version = serialized[0];
-    if (version == SEED_FILE_VERSION_1) {
-      final byte[] salt = readSalt_v1(serialized);
-      final AesCbcWithIntegrity.CipherTextIvMac civ = new AesCbcWithIntegrity.CipherTextIvMac(
-        readCipher_v1(serialized), readIV_v1(serialized), readMAC_v1(serialized));
-      return new EncryptedSeed(version, salt, civ);
-    } else {
-      throw new RuntimeException("unhandled encrypted seed file version");
-    }
-  }
-
-  /* --- utility methods for seed serialization version 1 --- */
-
-  private static byte[] readSalt_v1(byte[] content) {
-    final byte[] salt = new byte[SALT_LENGTH_V1];
-    System.arraycopy(content, VERSION_LENGTH, salt, 0, SALT_LENGTH_V1);
-    return salt;
-  }
-
-  private static byte[] readIV_v1(byte[] content) {
-    final byte[] iv = new byte[IV_LENGTH_V1];
-    System.arraycopy(content, VERSION_LENGTH + SALT_LENGTH_V1, iv, 0, IV_LENGTH_V1);
-    return iv;
-  }
-
-  private static byte[] readMAC_v1(byte[] content) {
-    final byte[] mac = new byte[MAC_LENGTH_V1];
-    System.arraycopy(content, VERSION_LENGTH + SALT_LENGTH_V1 + IV_LENGTH_V1, mac, 0, MAC_LENGTH_V1);
-    return mac;
-  }
-
-  private static byte[] readCipher_v1(byte[] content) {
-    final int cipherLength = content.length - VERSION_LENGTH - SALT_LENGTH_V1 - IV_LENGTH_V1 - MAC_LENGTH_V1;
-    final byte[] cipher = new byte[cipherLength];
-    System.arraycopy(content, VERSION_LENGTH + SALT_LENGTH_V1 + IV_LENGTH_V1 + MAC_LENGTH_V1, cipher, 0, cipherLength);
-    return cipher;
   }
 
 }
