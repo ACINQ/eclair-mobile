82fb90d7940af1186c3a8c241e92e54abbefcf22
dpad85 <dpadiou@gmail.com>
2019-01-25 16:49:25 +0100

    Increased payment request generation timeout

--- app/src/main/java/fr/acinq/eclair/wallet/App.java
+++ app/src/main/java/fr/acinq/eclair/wallet/App.java
@@ -196,13 +196,13 @@ public class App extends Application {
   }
 
   /**
-   * Generates a payment request. Uses a blocking await so must *not* be called from an UI thread. Fails after 5 secs.
+   * Generates a payment request. Uses a blocking await so must *not* be called from an UI thread. Fails after 20 secs.
    */
   public PaymentRequest generatePaymentRequest(final String description, final Option<MilliSatoshi> amountMsat_opt, final long expiry) throws Exception {
     Future<Object> f = Patterns.ask(appKit.eclairKit.paymentHandler(),
       new PaymentLifecycle.ReceivePayment(amountMsat_opt, description, Option.apply(expiry), NodeSupervisor.getRoutes()),
-      new Timeout(Duration.create(10, "seconds")));
-    return (PaymentRequest) Await.result(f, Duration.create(5, "seconds"));
+      new Timeout(Duration.create(20, "seconds")));
+    return (PaymentRequest) Await.result(f, Duration.create(30, "seconds"));
   }
 
   /**
