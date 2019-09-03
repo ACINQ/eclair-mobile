664f3e149d9db38ef7d7fa10920ca110d850d516
dpad85 <5765435+dpad85@users.noreply.github.com>
2019-09-03 13:14:47 +0200

    Payment expiry should also be checked when actually sending payment
    If users pauses the app and resumes the payment much later, the payment
    may have expired in the mean time and we should check that accordingly.
    
    Fixes #209

--- app/src/main/java/fr/acinq/eclair/wallet/activities/SendPaymentActivity.java
+++ app/src/main/java/fr/acinq/eclair/wallet/activities/SendPaymentActivity.java
@@ -119,8 +119,7 @@ public class SendPaymentActivity extends EclairActivity {
       return false;
     }
     // check that the payment has not expired
-    long expirySecs = paymentRequest.expiry().isDefined() ? (Long) paymentRequest.expiry().get() : 60 * 60; // default expiry is 1 hour = 3600 seconds
-    if (paymentRequest.timestamp() + expirySecs < System.currentTimeMillis() / 1000) {
+    if (hasInvoiceExpired(paymentRequest)) {
       canNotHandlePayment(R.string.payment_ln_expiry_outdated);
       return false;
     }
@@ -156,6 +155,11 @@ public class SendPaymentActivity extends EclairActivity {
     mBinding.setEnableSendButton(isWalletReady);
     new Handler().postDelayed(this::checkWalletReady, 1000);
     return isWalletReady;
+  }
+
+  private boolean hasInvoiceExpired(final PaymentRequest pr) {
+    final long expirySecs = pr.expiry().isDefined() ? (Long) pr.expiry().get() : 60 * 60; // default expiry is 1 hour
+    return pr.timestamp() + expirySecs < System.currentTimeMillis() / 1000;
   }
 
   private void setupOnchainPaymentForm(final BitcoinURI bitcoinURI) {
@@ -320,6 +324,10 @@ public class SendPaymentActivity extends EclairActivity {
         final long amountMsat = CoinUtils.convertStringAmountToMsat(mBinding.amountEditableBtcValue.getText().toString(), preferredBitcoinUnit.code()).amount();
         if (amountMsat <= 0) {
           handlePaymentError(R.string.payment_error_amount_zero_or_less);
+          return;
+        }
+        if (hasInvoiceExpired(paymentRequest)) {
+          canNotHandlePayment(R.string.payment_ln_expiry_outdated);
           return;
         }
         if (isPinRequired()) {
